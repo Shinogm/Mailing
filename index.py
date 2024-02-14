c0105a62c6dd3cf2b77d3f246fa59a51a4dc8240
KikeDeAlba <likingvirus1@gmail.com>
2024-02-13 22:56:43 -0700

    Add Supabase client and environment variables

--- index.py
+++ index.py
@@ -1,4 +1,13 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from supabase.client import Client, create_client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL") or ''
+key: str = os.environ.get("SUPABASE_KEY") or ''
+supabase: Client = create_client(url, key)
 
 app = FastAPI()
 
